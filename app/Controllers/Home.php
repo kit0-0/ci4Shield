76a2f4ad99117727acd6f28a818d4f06112538d2
kiko <kt1767@gmail.com>
2024-03-24 02:49:21 +0530

    Add authentication routes and update email configuration

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,10 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $user =\auth()->user();
+        echo "Hello {$user->username}<br/>";
+        echo "<a href=' ".site_url('logout')." '>Logout</a>";
     }
 }
